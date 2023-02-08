decbb209b35927342460ddc73050086f1d889032
safiaread <safia.read@emory.edu>
2023-02-08 13:23:42 -0500

    Submit Quiz 2

--- src/quiz/quiz2.py
+++ src/quiz/quiz2.py
@@ -17,7 +17,7 @@ def hair_salon() -> DialogueFlow:
                     }
                 }
             },
-            '[{haircoloring, hair coloring, dye, hair dyed, dyed, bleach, bleached, bleaching, balayage, highlights}]': {
+            '[{haircoloring, haircolouring, hair colouring, hair coloring, dye, hair dyed, dyed, bleach, bleached, bleaching, balayage, highlights}]': {
                 '`Sure. What date and time are you looking for?`': {
                     '{[{wednesday, thursday, wed, weds, thur, thurs}, {10,11}, am],[wed, weds, wednesday, 1, pm]}': {
                         '`You\'re appointment is set. See you!`': 'end'
@@ -37,13 +37,9 @@ def hair_salon() -> DialogueFlow:
                         '`You\'re appointment is set. See you!`': 'end'
                     },
                     'error': {
-                        '`Sorry, that slot is not available for a perm. The times we have available are Friday at '
-                        '10 AM, 11 AM, 1 PM, and 2 PM or Saturday at 10 AM and 2 PM.`': {
-                            '{[{friday, saturday, fri, sat}, 2, pm], [{friday, saturday, fri, sat}, 10, am], [{friday, fri}, 11,am], [{friday, fri}, 1, pm]}': {  # FIX
-                                '`You\'re appointment is set. See you!`': 'end'
-                            },
+                        '`Sorry, that slot is not available for a haircut.`': {
                             'error': {
-                                '`Ok, have a good day.`': 'end'
+                                '`Goodbye`': 'end'
                             }
                         }
                     }
