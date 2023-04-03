ed73375545476837edce93431b0f6d38b96c4af3
safiaread <safia.read@emory.edu>
2023-04-03 00:53:30 -0400

    Submit Quiz 5

--- src/quiz/quiz5.py
+++ src/quiz/quiz5.py
@@ -10,10 +10,13 @@ from src.utils import MacroGPTJSON, MacroNLG
 
 PATH_USER_INFO = 'resources/userinfo.json'
 
+
 class V(Enum):
     call_names = 0,
     apt_type = 1
     apt_hours = 2
+
+
 def hair_salon() -> DialogueFlow:
     transitions = {
         'state': 'start',
@@ -50,8 +53,9 @@ def hair_salon() -> DialogueFlow:
     df.add_macros(macros)
     return df
 
+
 class MacroValid(Macro):
-   def run(self, ngrams: Ngrams, vars: Dict[str, Any], args: List[Any]):
+    def run(self, ngrams: Ngrams, vars: Dict[str, Any], args: List[Any]):
         valid = "Great, your appointment is booked!"
         invalid = "Sorry that time isn't available."
         if vars[V.apt_type.name] == "Hair Cut":
@@ -110,8 +114,55 @@ def set_apt_type_hours(vars: Dict[str, Any], user: Dict[str, Any]):
     vars[V.apt_type.name] = user[V.apt_type.name]
     vars[V.apt_hours.name] = {d['day']: [d['time']] for d in user[V.apt_hours.name]}
 
+
 # macro to check if appointment is valid
 
 if __name__ == '__main__':
     openai.api_key_path = utils.OPENAI_API_KEY_PATH
     hair_salon().run()
+
+
+# Quiz 5 Questions
+
+# What are the limitations of the Bag-of-Words representation?
+
+# Bag-of-Words takes up a lot of space because each word has a vector space. Since the bag of words has every value
+# that is not in the string set to 0, and the rest of the values set to the frequency that they occur. However,
+# frequency may be the most representative way to capture the meaning of words. Words that appear the most frequently
+# such as articles might not convey as much meaning as rarer words such as proper nouns. Additionally Bag of Words
+# does not preserve the order of strings, which could change the meaning of the sentence.
+
+# Describe the Chain Rule and Markov Assumption and how they are used to estimate the probability of a word sequence.
+
+# The chain rule is used to find out the probability that words in a string word occur together by multiplying the
+# probability of the first word by the probability the second word occurs given the first word multiplied by the
+# probability of the third word given the first word squared and continues this pattern to the nth word. However,
+# this will create a very low probability if there are many words in a string because most words do not occur in the
+# exact same lengthy order all the time. Therefore the Markov Assumption is employed, which assumes that the
+# probability that the previous word is related to the word is roughly the same as the probability of all the words
+# in a string being related.
+
+# Explain how the Word2Vec approach uses feed-forward neural networks to generate word embeddings. What are the
+# advantages of the Word2Vec representation over the Bag-of-Words representation?
+
+# Word2Vec uses Continuous Bag of Words and Skip-Gram distributional architectures. The Continuous Bag of Words
+# predicts what a word will be given the surrounding contextual words. This generates a score. The Skip-Gram
+# architecture is given a word and attempts to predict the contextual words. This gives the model training data to
+# use to become more accurate in generation. Some advantages of Word2Vec are that it does not look at all the data in
+# a string at the same time but focuses on the relationships between a word and its contextual words. Therefore it is
+# not reliant on frequency.
+
+# Explain what patterns are learned in the multi-head attentions of a Transformer. What are the advantages of the
+# Transformer embeddings over the Word2Vec embeddings?
+
+# The multi-head attention module learns the relationship between words by performing matrix multiplication on n by d
+# matrices where n is the vector of words and d is the number of dimensions. The multiplication and transformations
+# use weights that the model determines itself during training. This calculates the attention score which is the
+# weight between the ngrams. Each head of the transformer can learn different patterns such as local relationships
+# between the following and preceding words to a word as well as global relationships between words far apart in the
+# document or contextual patterns. It can also learn semantic meanings between words or the syntax. While Word2Vec
+# gets the context of a few words surrounding the word, multi-head attention is much more accurate because it can
+# process the relationship between each word and all the other words. This enables it to understand much more
+# context, especially of a longer document, whereas Word2Vec would not be able to form those connections. Word2Vec is
+# good at capturing local patterns but is not as good at capturing the rest of the patterns or a multiplicity of
+# complex relationships.
